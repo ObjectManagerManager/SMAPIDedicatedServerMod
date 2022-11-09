15a1e9b827af6100a62e8dfc4584f63d73019765
ObjectManagerManager <objectmanagermanager@gmail.com>
2022-11-09 15:29:04 -0800

    Update TODOs

--- DedicatedServer/ModEntry.cs
+++ DedicatedServer/ModEntry.cs
@@ -16,14 +16,19 @@ namespace DedicatedServer
         // TODO Add more config options, like the ability to disable the crop saver (perhaps still keep track of crops
         // in case it's enabled later, but don't alter them).
 
-        // TODO Implement chat commands to build cabins, move buildings, and destroy buildings. Normally only the host
-        // can do these things
+        // TODO Implement chat commands to move buildings, or overwrite the existing built-in building-moving permission.
+        // The former would introduce a carpenter-independent way of moving buildings, which could then be controlled
+        // strictly with server-side per-player permission configuration. The latter would allow non-hosts to move buildings
+        // via the carpenter UI. Ideally, a "complete" solution would enable the UI moving mechanic when no server-side
+        // permissions are configured (or when the "move building" action is permissible to everyone), and disable it
+        // otherwise, forcing players to use the strictly-controlled chat commands. But permission files don't need to
+        // be supported for v1.0.0, so maybe we'll just do the built-in UI permission override for now.
 
-        // TODO Implement unlimited players
+        // TODO Remove player limit (if the existing attempts haven't already succeeded in doing that).
         
-        // TODO Make host invisible
-
-        // TODO Make host add to Luau pot or change how the score is computed, if possible
+        // TODO Make the host invisible to everyone else
+        
+        // TODO Consider what the automated host should do when another player proposes to them.
 
         /*********
         ** Public methods
